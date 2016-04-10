2883612cf9a40431773182dc692706b0c27cbd28
dalmia <amandalmia18@gmail.com>
2016-04-11 02:01:36 +0530

    Functionality Added to buttons. Change editText value

--- app/src/main/java/com/amandalmia/calculator/MainActivity.java
+++ app/src/main/java/com/amandalmia/calculator/MainActivity.java
@@ -4,12 +4,20 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+/**
+ * MainActivity is where all the fun stuff takes place
+ * It now implements an interface - OnClickListener to
+ * handle the click events and make our code more
+ * readable.
+ */
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     //Declare your views here
     Button one;
+    EditText result;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,8 +26,10 @@ public class MainActivity extends AppCompatActivity {
         //(layout -> directory to search for the view)
         setContentView(R.layout.activity_main);
 
-        //Link the button with the respective button in the view
+        //Link the widgets with the respective views in the layout file
         one = (Button) findViewById(R.id.one);
+        result = (EditText) findViewById(R.id.calculation_result);
+
 
         //Set the click Listener
         one.setOnClickListener(new View.OnClickListener() {
@@ -37,5 +47,42 @@ public class MainActivity extends AppCompatActivity {
                 Toast.makeText(MainActivity.this, one.getText().toString(), Toast.LENGTH_SHORT).show();
             }
         });
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.one:
+                result.setText("1");
+                break;
+            case R.id.two:
+                result.setText("2");
+                break;
+            case R.id.three:
+                result.setText("3");
+                break;
+            case R.id.four:
+                result.setText("4");
+                break;
+            case R.id.five:
+                result.setText("5");
+                break;
+            case R.id.six:
+                result.setText("6");
+                break;
+            case R.id.seven:
+                result.setText("7");
+                break;
+            case R.id.eight:
+                result.setText("8");
+                break;
+            case R.id.nine:
+                result.setText("9");
+                break;
+            case R.id.zero:
+                result.setText("0");
+                break;
+
+        }
     }
 }
