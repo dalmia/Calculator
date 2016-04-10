fff04cef2e574d40bd3f8b93a19653cc7acabf72
dalmia <amandalmia18@gmail.com>
2016-04-11 02:13:17 +0530

    First operation - addition added

--- app/src/main/java/com/amandalmia/calculator/MainActivity.java
+++ app/src/main/java/com/amandalmia/calculator/MainActivity.java
@@ -5,7 +5,10 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import org.w3c.dom.Text;
 
 /**
  * MainActivity is where all the fun stuff takes place
@@ -17,7 +20,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     //Declare your views here
     Button one;
-    EditText result;
+    TextView result;
+    String operation = "", firstVariable, secondVariable = "";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,7 +33,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         //Link the widgets with the respective views in the layout file
         one = (Button) findViewById(R.id.one);
-        result = (EditText) findViewById(R.id.calculation_result);
+        result = (TextView) findViewById(R.id.calculation_result);
 
 
         //Set the click Listener
@@ -54,34 +59,95 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         switch (v.getId()){
             case R.id.one:
                 result.setText("1");
+                if(!operation.equals(""))
+                    secondVariable = "1";
                 break;
             case R.id.two:
                 result.setText("2");
+                if(!operation.equals(""))
+                    secondVariable = "2";
                 break;
             case R.id.three:
                 result.setText("3");
+                if(!operation.equals(""))
+                    secondVariable = "3";
                 break;
             case R.id.four:
                 result.setText("4");
+                if(!operation.equals(""))
+                    secondVariable = "4";
                 break;
             case R.id.five:
                 result.setText("5");
+                if(!operation.equals(""))
+                    secondVariable = "5";
                 break;
             case R.id.six:
                 result.setText("6");
+                if(!operation.equals(""))
+                    secondVariable = "6";
                 break;
             case R.id.seven:
                 result.setText("7");
+                if(!operation.equals(""))
+                    secondVariable = "7";
                 break;
             case R.id.eight:
                 result.setText("8");
+                if(!operation.equals(""))
+                    secondVariable = "8";
                 break;
             case R.id.nine:
                 result.setText("9");
+                if(!operation.equals(""))
+                    secondVariable = "9";
                 break;
             case R.id.zero:
                 result.setText("0");
+                if(!operation.equals(""))
+                    secondVariable = "0";
+
+            case R.id.add:
+                operation = "add";
+                firstVariable = result.getText().toString();
+
+            case R.id.subtract:
+                operation = "subtract";
+                firstVariable = result.getText().toString();
+
+            case R.id.multiply:
+                operation = "multiply";
+                firstVariable = result.getText().toString();
+
+            case R.id.divide:
+                operation = "divide";
+                firstVariable = result.getText().toString();
                 break;
+
+            case R.id.equal:
+                switch (operation){
+                    case "add":
+                        if(!secondVariable.equals("")) {
+                            int addition = Integer.valueOf(firstVariable) + Integer.valueOf(secondVariable);
+                            result.setText(addition);
+                        }
+                        operation = "";
+                        secondVariable = "";
+                        break;
+
+                    case "subtract":
+                        break;
+
+                    case "multiply":
+                        break;
+
+                    case "divide":
+                        break;
+
+                    default:break;
+                }
+                break;
+
 
         }
     }
